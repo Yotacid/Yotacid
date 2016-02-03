14bc7a3e9c448488da31eeca28687e04cc57d583
Yotacid <Yotacid@live.com>
2016-02-03 13:31:48 +0100

    index.php

--- www/index.php
+++ www/index.php
@@ -4,7 +4,18 @@
 
 
 
+	DEFINE('DB_USER','root');  
+	DEFINE('DB_HOST','localhost');  
+	DEFINE('DB_PASSWORD','dxuxljgtf');  
+	DEFINE('DB_DATABASE','test');  
 
+
+
+
+
+
+
+	$dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);  
 
 
 ?>
